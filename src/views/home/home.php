dfd8650f90945ab4126b2943163ff4de76c3daa7
Louise Lalu <louiserafaellalu@gmail.com>
2023-07-30 11:13:17 +0800

    finalize color and font constants

--- src/views/home/home.php
+++ src/views/home/home.php
@@ -4,11 +4,25 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/style.css">
+    <link rel="stylesheet" href="src/views/home/home.css">
     <title>Diligens | Home</title>
 </head>
 
 <body>
     <?php include COMPONENTS_PATH . '/navbar/navbar.html' ?>
+    <div class="flex-container-row">
+        <img src="assets/illustration1.svg" alt="illustration 1" class="illustration">
+        <div class="flex-expand"></div>
+        <div class="color-green-700">
+            <p class="text-style-display text-style-size-60 text-style-weight-bold no-margin">
+                We are
+                <span class="text-style-italic">HERE</span>
+                to help!
+            </p>
+            <p class="text-style-display text-style-size-28 text-style-weight-bold no-margin">Empowering your success with expert guidance—we got you!</p>
+        </div>
+    </div>
 </body>
 
 </html>
