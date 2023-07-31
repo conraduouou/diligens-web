7df9f6a3763aaf4403880bc0e2f83162305cdec4
Louise Lalu <louiserafaellalu@gmail.com>
2023-07-31 15:12:00 +0800

    move about us and home views to the new pages directory

--- inc/config.php
+++ inc/config.php
@@ -2,5 +2,5 @@
 define("PROJECT_ROOT_PATH", "/diligens_web");
 define("BASE_URL_PATH", PROJECT_ROOT_PATH . "/index.php");
 define("COMPONENTS_PATH", "src/views/components");
-define("VIEWS_PATH", "src/views");
+define("VIEWS_PATH", "src/views/pages");
 ?>
--- index.php
+++ index.php
@@ -6,8 +6,8 @@ $uri = $_SERVER['REQUEST_URI'];
 $requestPath = strtok($uri, '?');
 
 $routes = [
-    BASE_URL_PATH => 'src/views/home/home.php',
-    BASE_URL_PATH . '/about-us' => 'src/views/about_us/about_us.php',
+    BASE_URL_PATH => VIEWS_PATH . '/home/home.php',
+    BASE_URL_PATH . '/about-us' => VIEWS_PATH . '/about_us/about_us.php',
 ];
 
 if (array_key_exists($requestPath, $routes)) {
