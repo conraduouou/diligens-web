732956af76abeb6b9a75bbfd25edeb8ccf9d8a0f
Louise Lalu <louiserafaellalu@gmail.com>
2023-07-31 16:49:16 +0800

    put navbar inside body

--- src/views/components/navbar/navbar.php
+++ src/views/components/navbar/navbar.php
@@ -5,26 +5,24 @@
     <link rel="stylesheet" href="<?php echo PROJECT_ROOT_PATH . '/' . COMPONENTS_PATH . '/navbar/navbar.css' ?>">
 </head>
 
-<header class="sticky">
-    <div class="navbar" id="myNavbar">
-        <nav>
-            <a href="/diligens_web/index.php">
-                <img class="diligens-logo" src="<?php echo PROJECT_ROOT_PATH . '/assets/logo.svg' ?>" alt="Logo"
-                    style="height: 80px;">
-                <div class="diligens">Diligens</div>
-            </a>
-            <div class="rightSection">
-                <a class="links" href="#">Services</a>
-                <a class="links" href="#">Rooms</a>
-                <a class="links" href="#">Stories</a>
-                <a class="links" href="/diligens_web/index.php/about-us">About Us</a>
-                <a class="links" href="/diligens_web/index.php/contact-us">Contact Us</a>
-        </nav>
-    </div>
-</header>
-
 <body>
-
+    <header class="sticky">
+        <div class="navbar" id="myNavbar">
+            <nav>
+                <a href="/diligens_web/index.php">
+                    <img class="diligens-logo" src="<?php echo PROJECT_ROOT_PATH . '/assets/logo.svg' ?>" alt="Logo"
+                        style="height: 80px;">
+                    <div class="diligens">Diligens</div>
+                </a>
+                <div class="rightSection">
+                    <a class="links" href="#">Services</a>
+                    <a class="links" href="#">Rooms</a>
+                    <a class="links" href="#">Stories</a>
+                    <a class="links" href="/diligens_web/index.php/about-us">About Us</a>
+                    <a class="links" href="/diligens_web/index.php/contact-us">Contact Us</a>
+            </nav>
+        </div>
+    </header>
 </body>
 
 </html>
