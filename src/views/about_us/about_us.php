f3050b2e14ea6fba39df3eeaa76f9fe5879b9d11
Louise Lalu <louiserafaellalu@gmail.com>
2023-07-31 13:15:34 +0800

    finish title section

--- src/views/about_us/about_us.php
+++ src/views/about_us/about_us.php
@@ -13,7 +13,17 @@
     <?php
     include COMPONENTS_PATH . '/navbar/navbar.php';
     ?>
-    <div></div>
+    <div class="title-section bg-color-green-800">
+        <h1
+            class="title-section-header text-style-display text-style-weight-bold text-style-size-60 color-white margin-none">
+            About us
+        </h1>
+        <div class="title-section-rectangle-container">
+            <div class="title-section-rectangle bg-color-green-600"></div>
+            <div class="title-section-rectangle bg-color-green-600"></div>
+        </div>
+    </div>
+    <div class="info-section"></div>
     <?php
     include COMPONENTS_PATH . '/footer/footer.php';
     ?>
