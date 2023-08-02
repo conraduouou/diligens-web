428076c558378e0b8f617f4bb083b4bff56d03cf
Louise Lalu <louiserafaellalu@gmail.com>
2023-08-02 22:15:29 +0800

    finish stories retouch

--- src/views/pages/stories/stories.php
+++ src/views/pages/stories/stories.php
@@ -20,30 +20,57 @@
     ?>
 
     <div class="container-story">
-        <div class="box">
-            <img src="<?php echo PROJECT_ROOT_PATH . '/assets/story-illustration1.svg' ?>" alt="Image 1">
-            <h3>Sample Company</h3>
-            <p>
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
-                industry's standard dummy text ever since the 1500s.
-            </p>
-        </div>
-        <div class="box">
-            <img src="<?php echo PROJECT_ROOT_PATH . '/assets/story-illustration2.svg' ?>" alt="Image 1">
-            <h3>Sample Company</h3>
-            <p>
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
-                industry's standard dummy text ever since the 1500s.
-            </p>
-        </div>
-        <div class="box">
-            <img src="<?php echo PROJECT_ROOT_PATH . '/assets/story-illustration3.svg' ?>" alt="Image 1">
-            <h3>Sample Company</h3>
-            <p>
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
-                industry's standard dummy text ever since the 1500s.
-            </p>
-        </div>
+        <a href="<?php echo BASE_URL_PATH . '/stories/1' ?>" class="container-story-box flex-container-col">
+            <div>
+                <img src="<?php echo PROJECT_ROOT_PATH . '/assets/story-image1.jpg' ?>" alt="Image 1">
+            </div>
+            <div class="container-story-details flex-container-col color-white">
+                <h3 class="text-style-display text-style-weight-bold text-style-size-36 color-green-100">
+                    Dow Inc.
+                </h3>
+                <p class="text-style-label text-style-weight-medium text-style-size-20 color-white margin-none">
+                    “From the outset, the Diligens team impressed us with their depth of industry knowledge and
+                    their genuine passion for empowering businesses.”
+                </p>
+                <span class="text-style-label text-style-weight-semibold text-style-size-18 color-green-300">
+                    READ MORE
+                </span>
+            </div>
+        </a>
+        <a href="<?php echo BASE_URL_PATH . '/stories/2' ?>" class="container-story-box flex-container-col">
+            <div>
+                <img src="<?php echo PROJECT_ROOT_PATH . '/assets/story-image2.jpg' ?>" alt="Image 1">
+            </div>
+            <div class="container-story-details flex-container-col color-white">
+                <h3 class="text-style-display text-style-weight-bold text-style-size-36 color-green-100">
+                    Endurah
+                </h3>
+                <p class="text-style-label text-style-weight-medium text-style-size-20 color-white margin-none">
+                    “Diligens impressed us with their deep expertise and genuine commitment to understanding our unique
+                    challenges and goals.”
+                </p>
+                <span class="text-style-label text-style-weight-semibold text-style-size-18 color-green-300">
+                    READ MORE
+                </span>
+            </div>
+        </a>
+        <a href="<?php echo BASE_URL_PATH . '/stories/3' ?>" class="container-story-box flex-container-col">
+            <div>
+                <img src="<?php echo PROJECT_ROOT_PATH . '/assets/story-image3.jpg' ?>" alt="Image 1">
+            </div>
+            <div class="container-story-details flex-container-col color-white">
+                <h3 class="text-style-display text-style-weight-bold text-style-size-36 color-green-100">
+                    Korri Inc.
+                </h3>
+                <p class="text-style-label text-style-weight-medium text-style-size-20 color-white margin-none">
+                    “As we aspired to grow and expand our reach, we recognized the need for expert guidance and
+                    strategic insights.”
+                </p>
+                <span class="text-style-label text-style-weight-semibold text-style-size-18 color-green-300">
+                    READ MORE
+                </span>
+            </div>
+        </a>
     </div>
 
     <div class="contact-section-stories">
