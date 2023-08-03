6beb789bba11b09f895f5f4827ac07e11ee1b37b
Louise Lalu <louiserafaellalu@gmail.com>
2023-08-03 20:59:47 +0800

    add purpose in message list

--- src/models/create_db.php
+++ src/models/create_db.php
@@ -20,7 +20,7 @@ try {
         $conn->select_db(DB_NAME);
 
         $conn->query("CREATE TABLE `" . ROOM_REQUEST_LIST . "` (`id` INT(50) NULL DEFAULT NULL , `name` VARCHAR(50) NULL DEFAULT NULL , `email` VARCHAR(50) NULL DEFAULT NULL , `number` VARCHAR(50) NULL DEFAULT NULL , `company` VARCHAR(50) NULL DEFAULT NULL , `room_id` INT(50) NULL DEFAULT NULL , `date` DATE NULL DEFAULT NULL ) ENGINE = InnoDB");
-        $conn->query("CREATE TABLE `" . MESSAGE_LIST . "` (`name` VARCHAR(50) NULL DEFAULT NULL , `email` VARCHAR(50) NULL DEFAULT NULL , `mobile` VARCHAR(50) NULL DEFAULT NULL , `company` VARCHAR(50) NULL DEFAULT NULL , `message` VARCHAR(300) NULL DEFAULT NULL ) ENGINE = InnoDB");
+        $conn->query("CREATE TABLE `" . MESSAGE_LIST . "` (`name` VARCHAR(50) NULL DEFAULT NULL , `email` VARCHAR(50) NULL DEFAULT NULL , `mobile` VARCHAR(50) NULL DEFAULT NULL , `company` VARCHAR(50) NULL DEFAULT NULL , `purpose` VARCHAR(50) NULL DEFAULT NULL , `message` VARCHAR(300) NULL DEFAULT NULL ) ENGINE = InnoDB");
         $conn->query("CREATE TABLE `" . ADMIN_USER_LIST . "` (`username` VARCHAR(50) NULL DEFAULT NULL , `password` VARCHAR(50) NULL DEFAULT NULL , `type` VARCHAR(50) NULL DEFAULT NULL ) ENGINE = InnoDB");
         $conn->query("CREATE TABLE `" . ROOM_LIST . "` (`id` INT(50) NULL DEFAULT NULL AUTO_INCREMENT KEY , `type` VARCHAR(50) NULL DEFAULT NULL , `cost` DECIMAL(50) NULL DEFAULT NULL , `media` VARCHAR(50) NULL DEFAULT NULL , `capacity_min` INT(50) NULL DEFAULT NULL, `capacity_max` INT(50) NULL DEFAULT NULL)");
         $conn->query("INSERT INTO `" . ADMIN_USER_LIST . "` (`username`, `password`, `type`) VALUES ('admin','admin','admin')");
