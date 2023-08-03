904cb18f179b60f75477e3468221b57b13b12776
Louise Lalu <louiserafaellalu@gmail.com>
2023-08-03 19:38:33 +0800

    fix no showing room details

--- src/controllers/pages/rooms/rooms.js
+++ src/controllers/pages/rooms/rooms.js
@@ -33,12 +33,42 @@ function radioBtnOnclick(index) {
     roomMedia.innerHTML = title(rooms[currentRoomIndex].media);
 }
 
+async function submitForm(event) {
+    event.preventDefault();
+
+    const formData = new FormData(form);
+    let body = '';
+
+    for (const pair of formData.entries()) {
+        if (pair[0] == 'type') {
+            body += 'room_id' + '=' + (currentRoomIndex + 1) + '&';
+        } else {
+            body += pair[0] + '=' + pair[1] + '&';
+        }
+    }
+
+    try {
+        const request = await fetch('/diligens_web/src/models/create_room_request.php', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+            body: body
+        });
+        const response = await request.text();
+        const jsonResponse = JSON.parse(response);
+        alert(jsonResponse.message);
+    } catch (error) {
+        console.error('Error: WHATS???!!', error);
+    }
+}
+
 document.addEventListener('DOMContentLoaded', async function () {
     // retrieve types
     await getRooms();
 
     const dropdownButton = document.getElementById('dropdown-button');
     dropdownButton.innerHTML = 'Type ' + rooms[currentRoomIndex].type;
+
+    radioBtnOnclick(currentRoomIndex);
 
     const dropdownOptions = document.getElementById('dropdown-option-container');
     for (let i = 0; i < rooms.length; i++) {
@@ -58,32 +88,6 @@ document.addEventListener('DOMContentLoaded', async function () {
 
     // add listener for when the form is submitted
     const form = document.getElementById('book-section-form');
-    form.addEventListener('submit', async function (event) {
-        event.preventDefault();
-
-        const formData = new FormData(form);
-        let body = '';
-
-        for (const pair of formData.entries()) {
-            if (pair[0] == 'type') {
-                body += 'room_id' + '=' + (currentRoomIndex + 1) + '&';
-            } else {
-                body += pair[0] + '=' + pair[1] + '&';
-            }
-        }
-
-        try {
-            const request = await fetch('/diligens_web/src/models/create_room_request.php', {
-                method: 'POST',
-                headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-                body: body
-            });
-            const response = await request.text();
-            const jsonResponse = JSON.parse(response);
-            alert(jsonResponse.message);
-        } catch (error) {
-            console.error('Error: WHATS???!!', error);
-        }
-    });
+    form.addEventListener('submit', submitForm);
 });
 
