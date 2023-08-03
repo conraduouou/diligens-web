b106f2d1438dabbf7bc057c6cdd26c33380ce61b
Louise Lalu <louiserafaellalu@gmail.com>
2023-08-03 18:54:52 +0800

    integrate get rooms script in rooms page

--- src/controllers/pages/rooms/rooms.js
+++ src/controllers/pages/rooms/rooms.js
@@ -1,32 +1,66 @@
-const roomTypes = ['A', 'B', 'C', 'D'];
+const rooms = [];
 let currentRoomIndex = 0;
 
 function wrapType(type, index) {
-    let toReturn = `<input type="radio" name="type" id="option${index}" ${index === currentRoomIndex ? `checked`: ``}>` +
+    let toReturn = `<input type="radio" name="type" id="option${index}" ${index === currentRoomIndex ? `checked` : ``}>` +
         `<label for="option${index}" class="dropdown-option text-style-label text-style-weight-medium text-style-size-18">`;
-    
+
     toReturn += 'Type ' + type;
     return toReturn;
 }
 
-document.addEventListener('DOMContentLoaded', function () {
+async function getRooms() {
+    const response = await fetch('/diligens_web/src/models/get_rooms.php', { method: 'GET' });
+    const text = await response.text();
+    const result = JSON.parse(text);
+    rooms.push(...result.body);
+}
+
+function title(str) {
+    return str.replace(/(^|\s)\S/g, function (t) { return t.toUpperCase() });
+}
+
+function radioBtnOnclick(index) {
+    const dropdownButton = document.getElementById('dropdown-button');
+    const roomCost = document.getElementById('room-cost');
+    const roomCapacity = document.getElementById('room-capacity');
+    const roomMedia = document.getElementById('room-media');
+
+    currentRoomIndex = index;
+    dropdownButton.innerHTML = 'Type ' + rooms[currentRoomIndex].type;
+    roomCost.innerHTML = rooms[currentRoomIndex].cost + ' php/hr';
+    roomCapacity.innerHTML = rooms[currentRoomIndex].capacity_min + '-' + rooms[currentRoomIndex].capacity_max + ' people';
+    roomMedia.innerHTML = title(rooms[currentRoomIndex].media);
+}
+
+document.addEventListener('DOMContentLoaded', async function () {
     // retrieve types
+    await getRooms();
 
     const dropdownButton = document.getElementById('dropdown-button');
-    dropdownButton.innerHTML = roomTypes[currentRoomIndex];
+    dropdownButton.innerHTML = 'Type ' + rooms[currentRoomIndex].type;
 
     const dropdownOptions = document.getElementById('dropdown-option-container');
-    for (let i = 0; i < roomTypes.length; i++) {
-        let type = roomTypes[i];
+    for (let i = 0; i < rooms.length; i++) {
+        let type = rooms[i].type;
         dropdownOptions.innerHTML += wrapType(type, i);
     }
 
     const radioButtons = document.getElementsByName('type');
     for (let i = 0; i < radioButtons.length; i++) {
         let button = radioButtons[i];
-        button.addEventListener('click', function() {
-            currentRoomIndex = i;
-            dropdownButton.innerHTML = roomTypes[currentRoomIndex];
-        });
+        button.addEventListener('click', () => radioBtnOnclick(i));
     }
+
+    const bookSection = document.getElementById('book-section');
+    bookSection.classList.remove('no-show');
+    bookSection.classList.add('show');
+
+    // add listener for when the form is submitted
+    const form = document.getElementById('book-section-form');
+    form.addEventListener('submit', function (event) {
+        event.preventDefault();
+
+    });
 });
+
--- src/views/pages/rooms/rooms.php
+++ src/views/pages/rooms/rooms.php
@@ -5,8 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="<?php echo PROJECT_ROOT_PATH . '/' . COMPONENTS_PATH . '/textfield/textfield.css' ?>">
-    <link rel="stylesheet"
-        href="<?php echo PROJECT_ROOT_PATH . '/' . COMPONENTS_PATH . '/primary_button/primary_button.css' ?>">
+    <link rel="stylesheet" href="<?php echo PROJECT_ROOT_PATH . '/' . COMPONENTS_PATH . '/primary_button/primary_button.css' ?>">
     <link rel="stylesheet" href="<?php echo PROJECT_ROOT_PATH . '/' . COMPONENTS_PATH . '/dropdown/dropdown.css' ?>">
     <link rel="stylesheet" href="<?php echo PROJECT_ROOT_PATH . '/' . VIEWS_PATH . '/rooms/rooms.css' ?>">
     <link rel="stylesheet" href="<?php echo PROJECT_ROOT_PATH . '/assets/style.css' ?>">
@@ -26,56 +25,49 @@
         Book a Room
     </h2>
 
-    <div class="book-section flex-container-row">
+    <div id="book-section" class="book-section flex-container-row no-show">
         <div class="book-section-left">
             <img src="<?php echo PROJECT_ROOT_PATH . '/assets/room-preview.png' ?> " alt="Room preview">
             <div class="book-section-room-details flex-container-col text-style-label text-style-size-28">
                 <div class="flex-container-row">
                     <span class="text-style-weight-semibold color-green-600">Rate:</span>
-                    <span class="text-style-weight-bold color-green-700">1,200 php/hr</span>
+                    <span id="room-cost" class="text-style-weight-bold color-green-700">1,200 php/hr</span>
                 </div>
                 <div class="flex-container-row">
                     <span class="text-style-weight-semibold color-green-600">Capacity:</span>
-                    <span class="text-style-weight-bold color-green-700">6-8 people</span>
+                    <span id="room-capacity" class="text-style-weight-bold color-green-700">6-8 people</span>
                 </div>
                 <div class="flex-container-row">
                     <span class="text-style-weight-semibold color-green-600">Projected media:</span>
-                    <span class="text-style-weight-bold color-green-700">Projector</span>
+                    <span id="room-media" class="text-style-weight-bold color-green-700">Projector</span>
                 </div>
             </div>
         </div>
         <div class="book-section-right">
             <form id="book-section-form" class="flex-container-col" method="POST">
                 <label for="name" class="textfield-label text-style-label">Name</label>
-                <input type="text" name="name" id="name" class="textfield text-style-label"
-                    placeholder="Put your beautiful name here..." required>
+                <input type="text" name="name" id="name" class="textfield text-style-label" placeholder="Put your beautiful name here..." required>
                 <label for="email" class="textfield-label text-style-label">Email</label>
-                <input type="email" name="email" id="email" class="textfield text-style-label"
-                    placeholder="Put your great-sounding email here..." required>
+                <input type="email" name="email" id="email" class="textfield text-style-label" placeholder="Put your great-sounding email here..." required>
                 <label for="number" class="textfield-label text-style-label">Number</label>
-                <input type="text" name="number" id="number" class="textfield text-style-label"
-                    placeholder="Put your mobile where we can contact you here..." required>
+                <input type="text" name="number" id="number" class="textfield text-style-label" placeholder="Put your mobile where we can contact you here..." required>
                 <label for="company" class="textfield-label text-style-label">Company name</label>
-                <input type="text" name="company" id="company" class="textfield text-style-label"
-                    placeholder="Put your mobile where we can contact you here..." required>
+                <input type="text" name="company" id="company" class="textfield text-style-label" placeholder="Put your mobile where we can contact you here..." required>
                 <label for="type" class="textfield-label text-style-label">Room type</label>
                 <div class="dropdown" tabindex="0">
-                    <div id="dropdown-button"
-                        class="text-style-label text-style-weight-medium text-style-size-18">
+                    <div id="dropdown-button" class="text-style-label text-style-weight-medium text-style-size-18">
                     </div>
                     <div id="dropdown-option-container">
                     </div>
                 </div>
                 <label for="date" class="textfield-label text-style-label">Date</label>
                 <input type="date" name="date" id="date" class="textfield text-style-label" required>
-                <input
-                    class="book-section-form-button primary-button color-white text-style-label text-style-weight-bold text-style-size-28"
-                    type="submit" value="Book room">
+                <input class="book-section-form-button primary-button color-white text-style-label text-style-weight-bold text-style-size-28" type="submit" value="Book room">
                 </input>
             </form>
         </div>
     </div>
-    
+
     <script src="<?php echo PROJECT_ROOT_PATH . '/' . CONTROLLERS_PATH . '/pages/rooms/rooms.js' ?>"></script>
 
     <?php
