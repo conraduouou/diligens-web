806e35c72731dcb255ad901e3150650ab16db467
Louise Lalu <louiserafaellalu@gmail.com>
2023-08-10 20:20:11 +0800

    refactor navbar code

--- src/views/components/navbar/navbar.php
+++ src/views/components/navbar/navbar.php
@@ -6,23 +6,21 @@
 </head>
 
 <body>
-    <header class="sticky">
-        <div class="navbar">
-            <nav>
-                <a href="/diligens_web/index.php">
-                    <img class="diligens-logo" src="<?php echo PROJECT_ROOT_PATH . '/assets/logo.svg' ?>" alt="Logo"
-                        style="height: 80px;">
-                    <div class="diligens">Diligens</div>
-                </a>
-                <div class="right-section">
-                    <a class="links" href="/diligens_web/index.php/stories">Stories</a>
-                    <a class="links" href="/diligens_web/index.php/about-us">About Us</a>
-                    <a class="links" href="/diligens_web/index.php/contact-us">Contact Us</a>
-                    <a class="links" href="/diligens_web/index.php/services">Services</a>
-                    <a class="links" href="/diligens_web/index.php/rooms">Rooms</a>
-                    <a class="links" href="/diligens_web/index.php#appointment">Appointment</a>
-            </nav>
-        </div>
+    <header class="navbar">
+        <nav>
+            <a class="diligens" href="/diligens_web/index.php">
+                <img class="diligens-logo" src="<?php echo PROJECT_ROOT_PATH . '/assets/logo.svg' ?>" alt="Logo" style="height: 80px;">
+                <h2>Diligens</h2>
+            </a>
+            <div class="right-section">
+                <a class="link" href="/diligens_web/index.php/stories">Stories</a>
+                <a class="link" href="/diligens_web/index.php/about-us">About Us</a>
+                <a class="link" href="/diligens_web/index.php/contact-us">Contact Us</a>
+                <a class="link" href="/diligens_web/index.php/services">Services</a>
+                <a class="link" href="/diligens_web/index.php/rooms">Rooms</a>
+                <a class="link" href="/diligens_web/index.php#appointment">Appointment</a>
+            </div>
+        </nav>
     </header>
 </body>
 
