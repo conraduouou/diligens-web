091191db5c6eb0834733f3cd9b74a208910a2e79
Louise Lalu <louiserafaellalu@gmail.com>
2023-08-10 22:08:03 +0800

    add responsive support for navbar

--- src/views/components/navbar/navbar.php
+++ src/views/components/navbar/navbar.php
@@ -7,20 +7,23 @@
 
 <body>
     <header class="navbar">
-        <nav>
+        <div>
             <a class="diligens" href="/diligens_web/index.php">
                 <img class="diligens-logo" src="<?php echo PROJECT_ROOT_PATH . '/assets/logo.svg' ?>" alt="Logo" style="height: 80px;">
                 <h2>Diligens</h2>
             </a>
-            <div class="right-section">
-                <a class="link" href="/diligens_web/index.php/stories">Stories</a>
-                <a class="link" href="/diligens_web/index.php/about-us">About Us</a>
-                <a class="link" href="/diligens_web/index.php/contact-us">Contact Us</a>
-                <a class="link" href="/diligens_web/index.php/services">Services</a>
-                <a class="link" href="/diligens_web/index.php/rooms">Rooms</a>
-                <a class="link" href="/diligens_web/index.php#appointment">Appointment</a>
-            </div>
-        </nav>
+            <button class="right-section-button">
+                <img src="<?= PROJECT_ROOT_PATH . '/assets/ic-burger.svg' ?>" alt="expand links icon">
+            </button>
+        </div>
+        <div class="right-section-links">
+            <a class="link" href="/diligens_web/index.php/stories">Stories</a>
+            <a class="link" href="/diligens_web/index.php/about-us">About Us</a>
+            <a class="link" href="/diligens_web/index.php/contact-us">Contact Us</a>
+            <a class="link" href="/diligens_web/index.php/services">Services</a>
+            <a class="link" href="/diligens_web/index.php/rooms">Rooms</a>
+            <a class="link" href="/diligens_web/index.php#appointment">Appointment</a>
+        </div>
     </header>
 </body>
 
